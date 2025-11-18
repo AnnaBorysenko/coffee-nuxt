4f79fd9d6faeb6be211064a5b14ba5c8facf19c8
Anna Borysenko <hi.borysenko.anna@gmail.com>
2025-11-18 15:00:24 +0200

    TASK-001: update app header

---
+++ app/app.vue
@@ -0,0 +1,12 @@
+<!--<template>-->
+<!--  <div>-->
+<!--    <NuxtRouteAnnouncer />-->
+<!--    <NuxtWelcome />-->
+<!--  </div>-->
+<!--</template>-->
+<template>
+  <div>
+    <h1>Hi ..</h1>
+    <p>Pet project on Nuxt.js</p>
+  </div>
+</template>
